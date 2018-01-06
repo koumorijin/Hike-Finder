8eff995e9bd34dcf4aede03587c410b6959dd164
Schuyler Van Ark <schuyler.vanark@gmail.com>
2018-01-06 15:47:36 -0700

    boom

--- assets/script.js
+++ assets/script.js
@@ -44,33 +44,31 @@ $(document).ready(function(){
 	//FUNCTION TO GET THE LATITIUDE AND LONGITUDE OF THE CITY ENTERED
 
 
-	// GET THE MODAL
-	var modal = $('#myModal');
+// Get the modal
+var modal = document.getElementById('myModal');
 
-	// GET THE BUTTON THAT OPENS THE MODAL 
-	var btn = $("#myBtn");
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-	// GET THE <span> ELEMENT THAT CLOSES THE MODAL
-	var span = $(".close")[0];
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-	// WHEN THE USER CLICKS THE BUTTON OPEN THE MODAL 
-	btn.onclick = function() {
-	    modal.style.display = "block";
-	}
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
 
-	// WHEN THE USER CLICKS THE <span> (x), CLOSE THE MODAL
-	span.onclick = function() {
-	    modal.style.display = "none";
-	}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
 
-	// WHEN THE USER CLICKS ANYWHERE OUTSIDE THE MODAL, CLOSE IT
-	window.onclick = function(event) {
-	    if (event.target == modal) {
-	        modal.style.display = "none";
-	    }
-	}
-});
-
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 // $(window).load(function() {
 //     // Construct the query string
 //     url = `https://www.mountainproject.com/data/get-user?email=essey@colorado.edu&key=200198519-332ea7ee0ec59a89221352f4463bc06b`
