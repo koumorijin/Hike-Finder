abc8d1b30171aa9fb988b378e36bb6cf3a3e5c7b
Schuyler Van Ark <schuyler.vanark@gmail.com>
2018-01-06 13:01:40 -0700

    get to work

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 	      "$$app_token" : "bRM4Mi2HNqY1XERoo2TVouQ67E"
 	    }
 	}).done(function(data) {
-	  alert("Retrieved " + data.length + " records from the dataset!");
+	  // alert("Retrieved " + data.length + " records from the dataset!");
 	  console.log(data);
 	});
 });
@@ -40,3 +40,29 @@ $(document).ready(function(){
 //           });
 //     });
 // });
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
