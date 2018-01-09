f553c32eba05c8b5e431f156b8b09bab86b131de
James Hafer <jhafer25@gmail.com>
2018-01-08 17:41:30 -0700

    JS Updates

--- assets/script.js
+++ assets/script.js
@@ -11,17 +11,12 @@ $(document).ready(function(){
   };
   firebase.initializeApp(config);
 
-  let city = "";
-  let state = "";
-  let radius = 0;
-  let cityLat = 0;
-  let cityLon = 0;
-
   	//ON CLICK SUBMIT BUTTON EVENT
   	$("#getHike").on("click", function() {
-		city = $("#city").val();
-		radius = $("#radius").val();
-		state = $("#state").val();
+		let city = $("#city").val();
+		let state = $("#state").val();
+		let radius = $("input#radius").val();
+		let resultsDesired = $("input#resultsDesired").val();
 		console.log(city);
 		console.log(radius);
 		address = city +', ' +state;
@@ -34,11 +29,11 @@ $(document).ready(function(){
 			}).done(function(data) {
 			console.log('googleapis');
 	  		console.log(data);
-	  		currentLat = data.results[0].geometry.location.lat;
-	  		currentLong = data.results[0].geometry.location.lng;
+	  		let currentLat = data.results[0].geometry.location.lat;
+	  		let currentLong = data.results[0].geometry.location.lng;
 	  		$.ajax({
 		   	 //url: "https://www.hikingproject.com/data/get-trails?lat="+cityLon+"&lon="+cityLon+"&maxDistance="+radius+"&key=200198519-a331d25bdc7ef2acc53d1f1636e3cd3b&maxResults=100",
-		   	 url: "https://www.hikingproject.com/data/get-trails?lat="+currentLat+"&lon="+currentLong+"&maxDistance=20&key=200198519-a331d25bdc7ef2acc53d1f1636e3cd3b&maxResults=100",
+		   	 url: "https://www.hikingproject.com/data/get-trails?lat="+currentLat+"&lon="+currentLong+"&maxDistance="+radius+"&key=200198519-a331d25bdc7ef2acc53d1f1636e3cd3b&maxResults="+resultsDesired+"",
 		    	type: "GET"
 				}).done(function(data) {
 		  		console.log(data);
@@ -76,31 +71,4 @@ $(document).ready(function(){
 	    }
 	}
 });
-// $(window).load(function() {
-//     // Construct the query string
-//     url = `https://www.mountainproject.com/data/get-user?email=essey@colorado.edu&key=200198519-332ea7ee0ec59a89221352f4463bc06b`
-//     // url = 'https://data.ct.gov/resource/v4tt-nt9n.json?'
-//     //       + 'organization_type=Public%20School%20Districts'
-//     //       + '&$$app_token=09sIcqEhoY0teGY5rhupZGqhW';
-    
-//     // Intialize our map
-//     var center = new google.maps.LatLng(41.7656874,-72.680087);
-//     var mapOptions = {
-//       zoom: 8,
-//       center: center
-//     }
-//     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    
-//     // Retrieve our data and plot it
-//     $.getJSON(url, function(data, textstatus) {
-//           $.each(data, function(i, entry) {
-//               var marker = new google.maps.Marker({
-//                   position: new google.maps.LatLng(entry.location_1.coordinates[1], 
-//                                                    entry.location_1.coordinates[0]),
-//                   map: map,
-//                   title: location.name
-//               });
-//           });
-//     });
-// });
 
